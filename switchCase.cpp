44473632e53a9627d951c6dde3c4d877e627b31d
hermayani2786 <herma.yani2786@gmail.com>
2026-03-31 20:10:32 +0700

    memasukan jurusan kampus

--- switchCase.cpp
+++ switchCase.cpp
@@ -12,3 +12,7 @@ void input() {
     cout << "1. Teknologi Informasi" << endl;
     cout << "2. Teknik Elektro" << endl;
     cout << "3. Sistem Informasi" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
