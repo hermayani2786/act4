84ae259935097619bf0cde2cfd9b2090a37a7e18
hermayani2786 <herma.yani2786@gmail.com>
2026-03-31 19:08:28 +0700

    menambahkan int main

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main(){
+    int x;
+
+    //Perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for( int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout <<endl;
+    //untuk mendapatkan urutan angka acak yang berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+}
