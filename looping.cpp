ffe3e4f105215d7b5da76a5a071087360fcb9f97
hermayani2786 <herma.yani2786@gmail.com>
2026-03-31 19:27:11 +0700

    menambahkan logika not

--- looping.cpp
+++ looping.cpp
@@ -19,4 +19,26 @@ int main(){
     cout<<"PERULANGAN WHILE"<<endl;
     x = 1 + rand()%10;
 
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    //Perulangan dengan DO...WHILE
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir ="<<x <<endl;
+    cout<<endl;
 }
+
+//Logika not
+#include <iostream>
+using namespace std;
